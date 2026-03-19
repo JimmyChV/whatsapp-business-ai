c827e99e4fa0913931ee9c75c832f9293dffebc0
Jimmy ChV <jchuquizuta@cleaning.com.pe>
2026-03-19 11:04:23 -0500

    refactor(backend): move tenant and security services into domain folders

--- backend/domains/security/index.js
+++ backend/domains/security/index.js
@@ -1,7 +1,7 @@
-﻿module.exports = {
+module.exports = {
     authService: require('../../auth_service'),
     authRecoveryService: require('../../auth_recovery_service'),
-    accessPolicyService: require('../../access_policy_service'),
+    accessPolicyService: require('./access-policy.service'),
     planLimitsService: require('../../plan_limits_service'),
     planLimitsStoreService: require('../../plan_limits_store_service'),
     auditLogService: require('../../audit_log_service')
