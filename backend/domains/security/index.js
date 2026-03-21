aa95d35abda9f9ccad3eb3234afc08603eb09d69
Jimmy ChV <jchuquizuta@cleaning.com.pe>
2026-03-21 05:44:51 -0500

    refactor(security): extract auth route registrar

--- backend/domains/security/index.js
+++ backend/domains/security/index.js
@@ -4,5 +4,6 @@ module.exports = {
     accessPolicyService: require('./access-policy.service'),
     planLimitsService: require('../../plan_limits_service'),
     planLimitsStoreService: require('../../plan_limits_store_service'),
-    auditLogService: require('../../audit_log_service')
+    auditLogService: require('../../audit_log_service'),
+    registerSecurityAuthHttpRoutes: require('./http-routes-auth').registerSecurityAuthHttpRoutes
 };
